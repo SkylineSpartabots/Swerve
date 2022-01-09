53cfdecceb89ef0e7b9eb1d9c44bd39e5934d901
Yanda Bao <yandabaodav@gmail.com>
2022-01-09 14:56:47 -0800

    Start of subsystems

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+
+public class ShooterSubsystem {
+
+    private static ShooterSubsystem instance = null;
+
+    /*
+    private final double maxInitialVelocity = (insert the equation found through encoder)
+    
+    private double distanceToHub = LimelightSubsystem.getDistance() or DrivetrainSubsystem.getOddometry()
+
+    private double distanceToTopBucket = distanceToHub * Math.sin(some angle, depending on height of shooter and the actual height of hub)
+    private double distanceToLowerBucket = distanceToHub * Math.sin(some angle, dependon on height of shooter and the actual height of hub)
+
+    */
+
+    
+
+    public static ShooterSubsystem getInstance(){
+        if(instance == null){
+            instance = new ShooterSubsystem();
+        }
+        return instance;
+    }
+
+
+
+    
+}
