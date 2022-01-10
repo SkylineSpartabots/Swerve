33cbdd7ad1e20b812aa0b227e7bcaff42e3bbe9b
Spencer Mundel <spencer.mundel@hotmail.com>
2022-01-09 17:00:53 -0800

    Fixed issues with shooter subsystem to build

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,11 +9,15 @@ import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -26,51 +30,55 @@ public final class Constants {
         public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 0; // FIXME Set front left module drive motor ID
         public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 1; // FIXME Set front left module steer motor ID
         public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 8; // FIXME Set front left steer encoder ID
-        public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(82.8781); // FIXME Measure and set front left steer offset
+        public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(82.8781); // FIXME Measure and set
+                                                                                              // front left steer offset
 
         public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2; // FIXME Set front right drive motor ID
         public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 3; // FIXME Set front right steer motor ID
         public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 9; // FIXME Set front right steer encoder ID
-        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(168.1320); // FIXME Measure and set front right steer offset
+        public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(168.1320); // FIXME Measure and set
+                                                                                                // front right steer
+                                                                                                // offset
 
         public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 4; // FIXME Set back left drive motor ID
         public static final int BACK_LEFT_MODULE_STEER_MOTOR = 5; // FIXME Set back left steer motor ID
         public static final int BACK_LEFT_MODULE_STEER_ENCODER = 10; // FIXME Set back left steer encoder ID
-        public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(123.4863); // FIXME Measure and set back left steer offset
+        public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(123.4863); // FIXME Measure and set
+                                                                                              // back left steer offset
 
         public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 6; // FIXME Set back right drive motor ID
         public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 7; // FIXME Set back right steer motor ID
         public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11; // FIXME Set back right steer encoder ID
-        public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(157.2363); // FIXME Measure and set back right steer offset
+        public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(157.2363); // FIXME Measure and set
+                                                                                               // back right steer
+                                                                                               // offset
 
-
-        
         public static final double kTrackWidth = 0.2921;
         // Distance between centers of right and left wheels on robot
         public static final double kWheelBase = 0.2921;
         // Distance between front and back wheels on robot
 
-        public static final SwerveDriveKinematics kDriveKinematics =
-            new SwerveDriveKinematics(
+        public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
                 new Translation2d(kWheelBase / 2, kTrackWidth / 2),
                 new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
                 new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
                 new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
 
-                public static final boolean kGyroReversed = false;
+        public static final boolean kGyroReversed = false;
 
         // These are example values only - DO NOT USE THESE FOR YOUR OWN ROBOT!
-        // These characterization values MUST be determined either experimentally or theoretically
+        // These characterization values MUST be determined either experimentally or
+        // theoretically
         // for *your* robot's drive.
-        // The SysId tool provides a convenient method for obtaining these values for your robot.
+        // The SysId tool provides a convenient method for obtaining these values for
+        // your robot.
         public static final double ksVolts = 1;
         public static final double kvVoltSecondsPerMeter = 0.8;
         public static final double kaVoltSecondsSquaredPerMeter = 0.15;
-    
+
         public static final double kMaxSpeedMetersPerSecond = 3;
     }
 
-    
     public static final class AutoConstants {
         public static final double kMaxSpeedMetersPerSecond = 3;
         public static final double kMaxAccelerationMetersPerSecondSquared = 3;
@@ -82,20 +90,27 @@ public final class Constants {
         public static final double kPThetaController = 1;
 
         // Constraint for the motion profilied robot angle controller
-        public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
-            new TrapezoidProfile.Constraints(
+        public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
                 kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
     }
 
-    public static final class TurnConstants{
+    public static final class TurnConstants {
         public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI * 4;
         public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI * 2;
         public static final double kPThetaController = 18.5;
         public static final double kIThetaController = 0;
         public static final double kDThetaController = 0.2;
 
-        public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
-            new TrapezoidProfile.Constraints(
+        public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
                 kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
     }
+
+    public static final class ShooterConstants {
+        // Shooter constants
+        public static final int SHOOTER_FLYWHEEL_MOTOR = 0;
+        public static final double SHOOTER_FLYWHEEL_HEIGHT = 5.0;
+        public static final double SHOOTER_FLYWHEEL_ANGLE_DEGREES = 80.0;
+        public static final double FALCON_VELO_RPM_FACTOR = 1195.0 / 600.0;
+    }
+
 }
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,69 +1,80 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
-import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.Constants.ShooterConstants;
+
 import java.lang.Math;
-
-import java.lang.module.ModuleDescriptor.Modifier;
-
 
 public class ShooterSubsystem extends SubsystemBase {
 
     private static ShooterSubsystem instance = null;
 
     /*
-    private final double maxInitialVelocity = (insert the equation found through encoder)
+     * private final double maxInitialVelocity = (insert the equation found through
+     * encoder)
+     * 
+     * private double distanceToHub = LimelightSubsystem.getDistance() or
+     * DrivetrainSubsystem.getOddometry()
+     * 
+     * private double distanceToTopBucket = distanceToHub * Math.sin(some angle,
+     * depending on height of shooter and the actual height of hub)
+     * private double distanceToLowerBucket = distanceToHub * Math.sin(some angle,
+     * dependon on height of shooter and the actual height of hub)
+     * 
+     */
 
-    private double distanceToHub = LimelightSubsystem.getDistance() or DrivetrainSubsystem.getOddometry()
-
-    private double distanceToTopBucket = distanceToHub * Math.sin(some angle, depending on height of shooter and the actual height of hub)
-    private double distanceToLowerBucket = distanceToHub * Math.sin(some angle, dependon on height of shooter and the actual height of hub)
-
-    */
-
-    public static ShooterSubsystem getInstance(){
-        if(instance == null){
-            instance = new ShooterSubsystem();
+    public static ShooterSubsystem getInstance() {
+        if (instance == null) {
+            instance = new ShooterSubsystem(desiredDistance);
         }
         return instance;
     }
-    
-    public ShooterSubsystem() {
 
-    }
-    
-    private ShuffleboardTab debugTab = ShuffleboardTab.getTab("Shooter");
+    private ShuffleboardTab debugTab = Shuffleboard.getTab("Shooter");
 
-    private final TalonFX flywheelTalon = new Talon(Constants.SHOOTER_FLYWHEEL_MOTOR);
+    private final TalonFX flywheelTalon = new TalonFX(ShooterConstants.SHOOTER_FLYWHEEL_MOTOR);
 
+    private static double desiredDistance = 0;
     private boolean ballShotState;
     private double ballShotTime;
     private double flywheelVelo = flywheelTalon.getSelectedSensorVelocity(0);
+    private double flywheelRpm = rawVeloToRpm(flywheelVelo);
+
+    public ShooterSubsystem(double desiredDistance) {
+        desiredDistance = ShooterSubsystem.desiredDistance;
+    }
 
     private double rawVeloToRpm(double velo) {
-        return velo / Constants.FALCON_VELO_RPM_FACTOR;
+        return velo / ShooterConstants.FALCON_VELO_RPM_FACTOR;
     }
-    
+
     private double rpmtoRadSec(double rpm) {
         return Units.rotationsPerMinuteToRadiansPerSecond(rpm);
     }
 
-    private double rpmToBallVeloX() {
-        return (this.rpmtoRadSec(rawVeloToRpm(flywheelVelo))*Math.cos(Units.degreesToRadians(Constants.SHOOTER_FLYWHEEL_ANGLE_DEGREES)-9.81*this.ballShotTime)
+    private double rpmToBallVeloX(double time) {
+        double velo = this.flywheelRpm;
+        velo = velo * (Math.cos(Units.degreesToRadians(ShooterConstants.SHOOTER_FLYWHEEL_ANGLE_DEGREES)));
+        velo = velo - 9.81;
+        velo = velo * time;
+        return velo;
     }
-    
-    private double rpmToBallVeloY() {
-        return (this.rpmtoRadSec(rawVeloToRpm(flywheelVelo))*Math.sin(Constants.SHOOTER_FLYWHEEL_ANGLE_DEGREES)-9.81*this.ballShotTime);
+
+    private double rpmToBallVeloY(double time) {
+        double velo = this.flywheelRpm;
+        velo = velo * (Math.sin(Units.degreesToRadians(ShooterConstants.SHOOTER_FLYWHEEL_ANGLE_DEGREES)));
+        velo = velo - 9.81;
+        velo = velo * time;
+        return velo;
     }
 
     private void ballShotTimer() {
         if (ballShotState) {
-            ballShotTime = ballShotTime+0.02;
+            ballShotTime = ballShotTime + 0.02;
         } else {
             ballShotTime = 0;
         }
@@ -72,12 +83,14 @@ public class ShooterSubsystem extends SubsystemBase {
     @Override
     public void periodic() {
         this.ballShotTimer();
+        debugTab.add("Flywheel RPM", flywheelRpm);
+        debugTab.add("Flywheel Rad/sec", this.rpmtoRadSec(flywheelRpm));
+        debugTab.add("Ball X Velocity", this.rpmToBallVeloX(this.ballShotTime));
+        debugTab.add("Ball Y Velocity", this.rpmToBallVeloY(this.ballShotTime));
     }
 
     @Override
     public void simulationPeriodic() {
         this.periodic();
     }
-
-    
 }
