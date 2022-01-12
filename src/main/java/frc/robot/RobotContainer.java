5a1ff0f005326d96e34d6f080a4219cf9019e3f8
victorxheng <victorchengthesecond@gmail.com>
2022-01-12 08:20:26 -0800

    working odo aim with cas

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -113,8 +113,8 @@ public class RobotContainer {
 
         
       double tx = findAngle(m_drivetrainSubsystem.getPose(), m_drivetrainSubsystem.getPose().getRotation().getDegrees(), 1, 0);
-      double heading_error = -tx;
-      double Kp = 0.05;
+      double heading_error = tx;
+      double Kp = 0.1;
       double maxSpeed = 2;
       double steering_adjust = Kp*heading_error;
         //Math.copySign(Math.pow(Math.abs(heading_error), 0.25),heading_error);//multiplies it by the root of the heading error, keeping sign
@@ -149,7 +149,14 @@ public class RobotContainer {
   public double findAngle(Pose2d currentPose, double heading, double toX, double toY){
     double deltaY = (toY - currentPose.getY());
     double deltaX = (toX - currentPose.getX());
+    double absolute = Math.toDegrees(Math.atan2(deltaY, deltaX));
+    double angle = currentPose.getRotation().getDegrees();
 
+    double result = absolute - angle;
+    if(Math.abs(result)>180){
+      result = -Math.copySign(360-Math.abs(result), result);
+    }
+    /*
     double absolute = Math.toDegrees(Math.atan2(deltaY, deltaX));
     if(absolute>0) absolute -= 180;
     else if(absolute < 0) absolute += 180;
@@ -162,7 +169,7 @@ public class RobotContainer {
     double difference2 = -1 * (difference1 % 180);//modifiedHeading - absolute;
     double result = Math.abs(difference1)>Math.abs(difference2) ? difference2 : difference1;
 
-    
+    */
     SmartDashboard.putNumber("currentY",currentPose.getY());
     SmartDashboard.putNumber("currentX",currentPose.getX());
     SmartDashboard.putNumber("deltaY",deltaY);
@@ -170,9 +177,6 @@ public class RobotContainer {
     SmartDashboard.putNumber("odo rotation",currentPose.getRotation().getDegrees());
     SmartDashboard.putNumber("navX rotation",heading);  
     SmartDashboard.putNumber("absolute angle",absolute);
-    SmartDashboard.putNumber("modified heading",modifiedHeading);
-    SmartDashboard.putNumber("difference 1",difference1);
-    SmartDashboard.putNumber("difference 2",difference2);
     SmartDashboard.putNumber("findAngle",result);  
 
 
