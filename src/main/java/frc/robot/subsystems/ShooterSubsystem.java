9b747813f28331fd8d889155d1bf0dd650dd4796
Spencer Mundel <spencer.mundel@hotmail.com>
2022-01-13 19:58:43 -0800

    Optimized ShooterSubsystem

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -3,9 +3,13 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
@@ -22,7 +26,6 @@ public class ShooterSubsystem extends SubsystemBase {
 
     */
     private final TalonFX flywheelTalon = new TalonFX(ShooterConstants.kFlywheelMotor);
-    private ShuffleboardTab debugTab = Shuffleboard.getTab("Shooter");
 
     public static ShooterSubsystem getInstance() {
         if (instance == null) {
@@ -35,17 +38,21 @@ public class ShooterSubsystem extends SubsystemBase {
         return velo / ShooterConstants.kFalconVeloRpmFactor;
     }
 
-    private double rpmtoRadSec(double rpm) {
-        return Units.rotationsPerMinuteToRadiansPerSecond(rpm);
-    }
-
     public void setMotorPower(double power) {
         flywheelTalon.set(ControlMode.PercentOutput, power);
     }
+    
+    private ShuffleboardTab debugTab = Shuffleboard.getTab("Shooter");
+    private NetworkTableInstance tableInstance = NetworkTableInstance.getDefault();
+    private NetworkTable table = tableInstance.getTable("Shooter");
 
     @Override
     public void periodic() {
-        debugTab.add((Sendable) flywheelTalon);
+        table.getEntry("Flywheel Talon Velocity").setDouble(flywheelTalon.getSelectedSensorVelocity());
+        table.getEntry("Flywheel Talon Power").setDouble(flywheelTalon.getMotorOutputPercent());
+        table.getEntry("Flywheel RPM").setDouble(rawVeloToRpm(flywheelTalon.getSelectedSensorVelocity()));
+        table.getEntry("Shooter On?").setBoolean(true);
+        table.getEntry("Shooter Subsystem").setValue(ShooterSubsystem.instance);
     }
 
     @Override
