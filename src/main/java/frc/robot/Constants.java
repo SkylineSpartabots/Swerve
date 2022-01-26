ae9e8a03c40846d8b081dd908344f19e38a87024
Ved Atal <ved1atal1@gmail.com>
2022-01-25 20:00:36 -0800

    shooter constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -101,6 +101,8 @@ public final class Constants {
 
     public static final class IntakeConstants {
         public static final int INTAKE_MOTOR_PORT = 0;
+        public static final double kIntakeOutSpeed = -0.5;
+        public static final double kIntakeInSpeed = 0.5;
     }
 
 	public static final double SHOOTER_FLYWHEEL_ANGLE_DEGREES = 0;
